8420feb90b40fba5eec3415771ef95ffb70e8ca3
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-07 16:31:49 +0530

    updte

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const express = require("express")
+const express = require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app= express()
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+  }));
+  app.use(bodyParser.json());
+  app.use("/",express.static("uploads"))
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 
 // config
@@ -12,5 +25,8 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   }
 
 
+
+  // it's for ErrorHandling
+app.use(ErrorHandler);
 
   module.exports = app;
