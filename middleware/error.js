486b68bf19cb110f2a396199be6827b280b3a6bd
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-07 13:25:30 +0530

    update

---
+++ middleware/error.js
@@ -0,0 +1,9 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal server Error";
+
+
+}
