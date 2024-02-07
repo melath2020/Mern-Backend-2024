ddd01b1d02696af2db0f1c2d1c39e5ddeca36f5b
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-07 12:04:40 +0530

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app= express()
+
+
+
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "config/.env",
+    });
+  }
+
+
+
+  module.exports = app;
