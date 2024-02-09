beba61da0297702798f408c55565063b8fe7790d
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-09 13:28:13 +0530

    updt

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,73 @@
 const express = require("express");
 const path=require('path');
+const { upload } = require("../multer");
+const ErrorHandler = require("../utils/ErrorHandler");
 const router = express.Router();
+const fs = require('fs');
+
+// create user
+router.post("/create-user",upload.single("file"),async(req,res,next)=>{
+    try{
+        const { name, email, password } = req.body;
+        const userEmail = await User.findOne({ email });
+    
+        if (userEmail) {
+          const filename=req.file.filename;
+          const filePath=`uploads/${filename}`;
+          fs.unlink(filePath,(err)=>{
+            if(err){
+              console.log(err);
+              res.status(500).json({
+                message:"Error Deleting file"
+              })
+
+            }
+          })
+            return next(new ErrorHandler("User already exists", 400));
+          }
+    
+          const filename=req.file.filename;
+          const fileUrl=path.join(filename);
+    
+          
+    
+          const user = {
+            name: name,
+            email: email,
+            password: password,
+            avatar:fileUrl
+          };
+    
+          const activationToken = createActivationToken(user);
+
+          const activationUrl = `${baseUrl}/activation/${activationToken}`;
+
+          try {
+            await sendMail({
+              email:user.email,
+              subject:"Activate Your Account",
+              message:`Hello ${user.name}, please click on the link to activate your account : ${activationUrl}`,
+            })
+            res.status(201).json({
+              success:true,
+              message:`please check your email:- ${user.email} to activate your account`
+            })
+            
+          } catch (error) {
+            return next(new ErrorHandler(error.message,500))
+            
+          }
+          
+      
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400));
+      }
+   
+})
+
+// create activation token
+const createActivationToken=(user) => {
+    return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+      expiresIn: "5m",
+    });
+  }
