37e889a0bc9b3cab4203028b91338267ef5d5fc3
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-10 17:59:44 +0530

    updte

--- controller/user.js
+++ controller/user.js
@@ -86,6 +86,40 @@ const createActivationToken=(user) => {
 }
 
 
+// activate user
+router.post("/activation",catchAsyncErrors(async(req,res,next)=>{
+  try {
+    const { activation_token } = req.body;
+
+    const newUser = jwt.verify(
+      activation_token,
+      process.env.ACTIVATION_SECRET
+    );
+
+    if (!newUser) {
+      return next(new ErrorHandler("Invalid token", 400));
+    }
+    const { name, email, password, avatar } = newUser;
+
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return next(new ErrorHandler("User already exists", 400));
+    }
+    user = await User.create({
+      name,
+      email,
+      avatar,
+      password,
+    });
+
+    sendToken(user, 201, res);
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+})
+);
+
 
 
 
