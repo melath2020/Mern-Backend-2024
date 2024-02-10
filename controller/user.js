a281a4756907ea54693ea5aa539351be1690bfa3
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-10 17:21:16 +0530

    upte

--- controller/user.js
+++ controller/user.js
@@ -6,52 +6,82 @@ const router = express.Router();
 const fs = require('fs');
 const User = require("../model/user");
 const jwt =require('jsonwebtoken');
-
+const sendToken = require("../utils/jwtToken");
 // create user
 router.post("/create-user",upload.single("file"),async(req,res,next)=>{
-    
-        const { name, email, password } = req.body;
-        const userEmail = await User.findOne({ email });
-    
-        if (userEmail) {
-          const filename=req.file.filename;
-          const filePath=`uploads/${filename}`;
-          fs.unlink(filePath,(err)=>{
-            if(err){
-              console.log(err);
-              res.status(500).json({
-                message:"Error Deleting file"
-              })
+    try {
+      
+      const { name, email, password } = req.body;
+      const userEmail = await User.findOne({ email });
+  
+      if (userEmail) {
+        const filename=req.file.filename;
+        const filePath=`uploads/${filename}`;
+        fs.unlink(filePath,(err)=>{
+          if(err){
+            console.log(err);
+            res.status(500).json({
+              message:"Error Deleting file"
+            })
 
-            }else{
-              res.json({
-                message:"File Deleted Successfully"
-              })
-            }
-          })
-            return next(new ErrorHandler("User already exists", 400));
+          }else{
+            res.json({
+              message:"File Deleted Successfully"
+            })
           }
-    
-          const filename=req.file.filename;
-          const fileUrl=path.join(filename);
-    
-          
-    
-          const user = {
-            name: name,
-            email: email,
-            password: password,
-            avatar:fileUrl
-          };
-    
-      console.log(user)
-      const newUser=await User.create(user);
+        })
+          return next(new ErrorHandler("User already exists", 400));
+        }
+  
+        const filename=req.file.filename;
+        const fileUrl=path.join(filename);
+  
+        
+  
+        const user = {
+          name: name,
+          email: email,
+          password: password,
+          avatar:fileUrl
+        };
+  
+    console.log(user)
+    const activationToken = createActivationToken(user);
+    const activationUrl = `${baseUrl}/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email:user.email,
+        subject:"Activate Your Account",
+        message:`Hello ${user.name}, please click on the link to activate your account : ${activationUrl}`,
+      })
       res.status(201).json({
         success:true,
-        newUser,
+        message:`please check your email:- ${user.email} to activate your account`
       })
+      
+    } catch (error) {
+      return next(new ErrorHandler(error.message,500))
+    }
+
+    } catch (error) {
+      return next(new ErrorHandler(error.message),400);
+    }
+      // const newUser=await User.create(user);
+      // res.status(201).json({
+      //   success:true,
+      //   newUser,
+      // })
    
 })
+
+
+// create activation token
+const createActivationToken=(user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+}
 
 
 
